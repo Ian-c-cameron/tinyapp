d8805c4d90bfbaa9c30942b6b25db47a282f1673
Ian-c-cameron <ian.c.cam@gmail.com>
2020-09-29 00:26:11 +0000

    added handling for input from the url form to put it in the urlDatabase object with a random 6 charectar string key

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -35,6 +37,24 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(`${shortURL}: ${req.body.longURL}`);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = function() {
+  let alph = "abcdefghijklmnopqrstuvwxyz";
+  alph = alph.split('');
+  let out = [];
+  for (let i = 0; i < 6; i++) {
+    out.push(alph[Math.floor(Math.random() * 26)]);
+  }
+  return out.join('');
+};
+
